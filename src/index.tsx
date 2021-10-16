956c9209613fc8da66239737f09c409b650c0617
Nathan Darrington <npdarrington@icloud.com>
2021-10-16 14:04:31 -0600

    Provide store to app

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+
+import './index.css';
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
