6ba953b5fbc43f5dc63ce231e5e6d0e12074e5fa
Nathan Darrington <npdarrington@icloud.com>
2021-10-16 07:19:49 -0600

    Style each ToDo and add Due By label for due date

--- src/components/ToDo/ToDo.tsx
+++ src/components/ToDo/ToDo.tsx
@@ -4,10 +4,10 @@ import './ToDo.css';
 
 const ToDo: React.FunctionComponent<IToDo> = ({ id, title, description, dueBy }) => {
   return (
-    <section className="to-do-card">
-      <h3 className="to-do-card__title">{title}</h3>
-      <h4 className="to-do-card__description">{description}</h4>
-      <h4 className="to-do-card__due-by">{dueBy}</h4>
+    <section className="todo-card">
+      <h3 className="todo-card__title">{title}</h3>
+      <h4 className="todo-card__description">{description}</h4>
+      <h4 className="todo-card__due-by">Due Date: {dueBy}</h4>
     </section>
   )
 } 
