491133b917451355fe8442e1135caf9d88ab9507
Nathan Darrington <npdarrington@icloud.com>
2021-10-16 14:21:47 -0600

    Add reset form on submit and assign values on inputs to state value, format file

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -6,48 +6,87 @@ import { useAppDispatch } from '../../redux/hooks';
 import './Form.css';
 
 export interface INewTodo {
-  title: string,
-  description: string,
-  dueBy: string
+	title: string;
+	description: string;
+	dueBy: string;
 }
 
 const Form = () => {
-  const [newTodo, setNewTodo] = useState<INewTodo>({
-    title: '',
-    description: '',
-    dueBy: ''
-  });
-  const [error, setError] = useState<string>('');
-  const dispatch = useAppDispatch();
+	const [newTodo, setNewTodo] = useState<INewTodo>({
+		title: '',
+		description: '',
+		dueBy: '',
+	});
+	const [error, setError] = useState<string>('');
+	const dispatch = useAppDispatch();
 
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
-    const { name, value } = event.target;
-    setNewTodo({ ...newTodo, [name]: value });
-  }
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+		const { name, value } = event.target;
+		setNewTodo({ ...newTodo, [name]: value });
+	};
 
-  const validateForm = (event: React.FormEvent<HTMLFormElement>): void => {
-    event.preventDefault();
-    if (!newTodo.title.trim() || !newTodo.description.trim() || !newTodo.dueBy.trim()) {
-      setError('You must fill out a title, description and due by date to add a new idea');
-      return;
-    }
-    dispatch(addNewIdea(newTodo));
-  }
+	const validateForm = (event: React.FormEvent<HTMLFormElement>): void => {
+		event.preventDefault();
+		if (
+			!newTodo.title.trim() ||
+			!newTodo.description.trim() ||
+			!newTodo.dueBy.trim()
+		) {
+			setError(
+				'You must fill out a title, description and due by date to add a new idea'
+			);
+			return;
+		}
+		dispatch(addNewIdea(newTodo));
+		setError('');
+		resetForm();
+	};
 
-  return (
-    <form className="new-todo__form" onSubmit={validateForm}>
-      <section className="new-todo__form-container">
-        <label htmlFor="title">Title:</label>
-        <input type="text" name="title" id="title" className="new-todo__form-input" onChange={handleChange} />
-        <label htmlFor="description">Description:</label>
-        <input type="text" name="description" id="description" className="new-todo__form-input" onChange={handleChange} />
-        <label htmlFor="dueBy">Due By:</label>
-        <input type="date" name="dueBy" id="dueBy" className="new-todo__form-input" onChange={handleChange} />
-        <button type="submit" className="new-todo__submit">Add To-Do!</button>
-      </section>
-      { error && <h2 className="new-todo__form-error error">{error}</h2>}
-    </form>
-  )
-}
+	const resetForm = () => {
+		setNewTodo({
+			title: '',
+			description: '',
+			dueBy: '',
+		});
+	};
+
+	return (
+		<form className='new-todo__form' onSubmit={validateForm}>
+			<section className='new-todo__form-container'>
+				<label htmlFor='title'>Title:</label>
+				<input
+					type='text'
+					name='title'
+					id='title'
+					value={newTodo.title}
+					className='new-todo__form-input'
+					onChange={handleChange}
+				/>
+				<label htmlFor='description'>Description:</label>
+				<input
+					type='text'
+					name='description'
+					id='description'
+					value={newTodo.description}
+					className='new-todo__form-input'
+					onChange={handleChange}
+				/>
+				<label htmlFor='dueBy'>Due By:</label>
+				<input
+					type='date'
+					name='dueBy'
+					id='dueBy'
+					value={newTodo.dueBy}
+					className='new-todo__form-input'
+					onChange={handleChange}
+				/>
+				<button type='submit' className='new-todo__submit'>
+					Add To-Do!
+				</button>
+			</section>
+			{error && <h2 className='new-todo__form-error error'>{error}</h2>}
+		</form>
+	);
+};
 
 export default Form;
