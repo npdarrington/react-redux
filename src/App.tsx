4fe8ac074883f9048faff82ba4c6c2efee8ae4a6
Nathan Darrington <npdarrington@icloud.com>
2021-10-16 07:43:07 -0600

    Render Form

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css';
 
+import Form from './components/Form/Form';
 import ToDoList from './containers/ToDoList/ToDoList';
 
 const App: React.FunctionComponent = () => {
   return (
     <main className="App">
       <h1>My To-Do List!</h1>
+      <Form />
       <ToDoList />
     </main>
   );
