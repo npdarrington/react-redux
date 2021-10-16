80272d00b9d5040254c61c95243b0adec0b853de
Nathan Darrington <npdarrington@icloud.com>
2021-10-16 09:52:33 -0600

    handleChange records user input

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -16,7 +16,12 @@ const Form = () => {
   });
   const [error, setError] = useState<string>('');
 
-  const validateForm = (event: React.FormEvent<HTMLFormElement>) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const { name, value } = event.target;
+    setNewTodo({ ...newTodo, [name]: value });
+  }
+
+  const validateForm = (event: React.FormEvent<HTMLFormElement>): void => {
     event.preventDefault();
     if (!newTodo.title.trim() || !newTodo.description.trim() || !newTodo.dueBy.trim()) {
       setError('You must fill out a title, description and due by date to add a new idea');
@@ -27,11 +32,11 @@ const Form = () => {
   return (
     <form className="new-todo__form" onSubmit={validateForm}>
       <label htmlFor="title">Title:</label>
-      <input type="text" name="title" id="title" className="new-todo__form-input" />
+      <input type="text" name="title" id="title" className="new-todo__form-input" onChange={handleChange} />
       <label htmlFor="description">Description:</label>
-      <input type="text" name="description" id="description" className="new-todo__form-input" />
+      <input type="text" name="description" id="description" className="new-todo__form-input" onChange={handleChange} />
       <label htmlFor="dueBy">Due By:</label>
-      <input type="datetime" name="dueBy" id="dueBy" className="new-todo__form-input" />
+      <input type="date" name="dueBy" id="dueBy" className="new-todo__form-input" onChange={handleChange} />
       <button type="submit" className="new-todo__submit">Add To-Do!</button>
       { error && <h2 className="new-todo__form-error">{error}</h2>}
     </form>
