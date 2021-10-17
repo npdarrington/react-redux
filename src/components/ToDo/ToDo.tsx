74152c55320906053d29c67ef298e61d092e6f99
Nathan Darrington <npdarrington@icloud.com>
2021-10-16 19:57:19 -0600

    dispatch deleteTodo on button click

--- src/components/ToDo/ToDo.tsx
+++ src/components/ToDo/ToDo.tsx
@@ -1,13 +1,20 @@
+import { useAppDispatch } from '../../redux/hooks';
+
+import { deleteTodo } from '../../redux/todoSlice';
+
 import { IToDo } from '../../containers/ToDoList/types';
 
 import './ToDo.css';
 
 const ToDo: React.FunctionComponent<IToDo> = ({ id, title, description, dueBy }) => {
+  const dispatch = useAppDispatch();
+  
   return (
     <section className="todo-card">
       <h3 className="todo-card__title">{title}</h3>
       <h4 className="todo-card__description">{description}</h4>
       <h4 className="todo-card__due-by">Due Date: {dueBy}</h4>
+      <button onClick={() => dispatch(deleteTodo(id))}>Delete todo</button>
     </section>
   )
 } 
